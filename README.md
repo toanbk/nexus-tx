f0d1bab5c27156d5a4e31a4d12cff42ff85b20d8
trangchongcheng <cheng.cit@gmail.com>
2025-02-22 17:33:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hướng dẫn
+
+Điền private key index.ts dòng 7 và lưu lại, sau đó chạy lần lượt các lệnh
+
+```shell
+npm install --force
+npm install -g typescript ts-node
+ts-node indext.ts
+```
---
+++ index.ts
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+const { Web3 } = require("web3");
+const RPC_URL = "https://rpc.nexus.xyz/http";
+const web3 = new Web3(RPC_URL);
+
+const PRIVATE_KEY = ""; // Nhập private key có 0x ở đầu
+const ACCOUNT = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY);
+const DELAY_TX = 10000; // 10 giây mỗi giao dịch
+const DELAY_TXS = 5 * 60 * 1000; // 5 phút chạy lại 1 lần
+
+async function getBalance(address: any) {
+  try {
+    const balance = await web3.eth.getBalance(address);
+    console.log(
+      `[Số dư ví:  ${address}:`,
+      web3.utils.fromWei(balance, "ether"),
+      "ETH]\n"
+    );
+    return balance;
+  } catch (error) {
+    console.error("Lỗi khi lấy số dư:", error);
+    return "0";
+  }
+}
+
+async function sendRandomNativeToken() {
+  await getBalance(ACCOUNT.address);
+  const timestamp = Date.now();
+  const url = `https://gist.githubusercontent.com/trangchongcheng/3f7769b1c3fb1a6f5d44a1fee8db5831/raw/?_=${timestamp}`;
+  const { data } = await axios.get(url);
+  const wallets = data;
+  for (let i = 0; i < wallets.length; i++) {
+    const recipientAddress = wallets[i];
+    const amount = web3.utils.toWei(
+      (0.01 + Math.random() * 0.15).toFixed(2),
+      "ether"
+    );
+
+    const tx = {
+      from: ACCOUNT.address,
+      to: recipientAddress,
+      value: amount,
+      gas: 21000,
+      gasPrice: (await web3.eth.getGasPrice()) * 5n,
+    };
+
+    try {
+      const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+      const receipt = await web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction
+      );
+      console.log(
+        `Giao dịch ${i + 1}/${wallets.length} thành công ${web3.utils.fromWei(
+          amount,
+          "ether"
+        )} $NEX to: ${recipientAddress}, tx: `,
+        receipt.transactionHash
+      );
+    } catch (error) {
+      console.error(`Lỗi khi gửi giao dịch ${i + 1}:`, error);
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, DELAY_TX));
+  }
+  console.log("Hoàn thành gửi token $NEX\n");
+  await getBalance(ACCOUNT.address);
+  console.log("============= ĐỢI 5 PHÚT CHẠY LẠI...==============\n\n");
+}
+setInterval(sendRandomNativeToken, DELAY_TXS);
